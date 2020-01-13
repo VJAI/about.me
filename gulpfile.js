bd7bd723fc040e174b32baa287eae09127a75042
Vijaya Anand <vanand@apttus.com>
2020-01-13 14:31:40 +0530

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var webserver = require('gulp-webserver');
+
+gulp.task('sass', function () {
+	return gulp.src(['./scss', '*.scss'].join('/'))
+	           .pipe(sass())
+	           .pipe(concat('aboutme.css'))
+	           .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/*.scss', ['sass']);
+});
+
+gulp.task('server', ['sass', 'watch'], function () {
+	gulp.src('.').pipe(
+		webserver(
+			{
+				port: 4200,
+				livereload: true,
+				directoryListing: false,
+				open: true
+			}
+		)
+	);
+});
+
+gulp.task('default', ['server']);
